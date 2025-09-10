835d6b1e7e74a8ddcbeba6ee7396197140430464
gatat123 <gatat123@naver.com>
2025-09-10 11:42:44 +0900

    fix: Remove unsupported Lenis options to fix build error
    - Removed normalizeWheel option
    - Removed touchInertiaMultiplier option
    - Removed touchInertia option
    
    These options are not available in the current version of Lenis library

--- components/ui/scroll-stack.tsx
+++ components/ui/scroll-stack.tsx
@@ -245,13 +245,10 @@ const ScrollStack = ({
         touchMultiplier: 2,
         infinite: false,
         gestureOrientation: 'vertical',
-        normalizeWheel: true,
         wheelMultiplier: 1,
-        touchInertiaMultiplier: 35,
         lerp: 0.1,
         syncTouch: true,
-        syncTouchLerp: 0.075,
-        touchInertia: 0.6
+        syncTouchLerp: 0.075
       });
 
       lenis.on('scroll', handleScroll);
